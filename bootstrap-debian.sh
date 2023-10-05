31c46dac4e8199a366fe1f12a75d93c55821f57c
soyflourbread <soyflourbread@gmail.com>
2023-10-04 18:11:01 -0700

    some updates.

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -1,5 +1,6 @@
 losetup -Pf fs.img
 
+printf "Create basic directory layout..."
 mkdir fs-mountpoint
 mount -o compress=zstd /dev/loop0p3 fs-mountpoint
 mkdir fs-mountpoint/efi
@@ -7,9 +8,18 @@ mkdir fs-mountpoint/boot
 mount /dev/loop0p1 fs-mountpoint/efi
 mount /dev/loop0p2 fs-mountpoint/boot
 
+printf "Bootstrapping..."
 debootstrap --arch arm64 testing fs-mountpoint http://deb.debian.org/debian/
 
 genfstab -t PARTUUID fs-mountpoint > fs-mountpoint/etc/fstab
+
+cp systemd-networkd/20-wired.network fs-mountpoint/etc/systemd/network/
+
+printf "Setting up auxiliary repo..."
+wget https://raw.githubusercontent.com/armbian/build/main/config/armbian.key
+gpg --dearmor < armbian.key > armbian.gpg
+cp armbian.gpg fs-mountpoint/etc/apt/keyrings/
+cp apt/armbian.list fs-mountpoint/etc/apt/sources.list.d/
 
 mount --bind /dev fs-mountpoint/dev
 mount -t proc proc fs-mountpoint/proc
--- configure-debian-chroot.sh
+++ configure-debian-chroot.sh
@@ -4,4 +4,10 @@ printf "Executing inside chroot..."
 
 printf "Install essential packages..."
 apt-get update
-apt-get -y install linux-image-arm64 btrfs-progs
+apt-get -y install wget btrfs-progs
+
+printf "Install Armbian RK3588 kernel..."
+apt-get install -y linux-image-edge-rockchip-rk3588 linux-dtb-edge-rockchip-rk3588
+
+printf "Configure GRUB2..."
+apt-get install -y grub-efi
