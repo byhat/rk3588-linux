030375bcde35f37b4411da62d9072ab5fc915b4e
soyflourbread <soyflourbread@gmail.com>
2023-10-04 19:17:08 -0700

    reformat.

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -13,12 +13,8 @@ debootstrap --arch arm64 testing fs-mountpoint http://deb.debian.org/debian/
 
 genfstab -t PARTUUID fs-mountpoint > fs-mountpoint/etc/fstab
 
-printf "Configuring networks..."
-cp systemd-networkd/20-wired.network fs-mountpoint/etc/systemd/network/
-ln -sf ../run/systemd/resolve/stub-resolv.conf fs-mountpoint/etc/resolv.conf
-
 printf "Setting up auxiliary repo..."
-wget https://raw.githubusercontent.com/armbian/build/main/config/armbian.key
+wget -O armbian.key https://raw.githubusercontent.com/armbian/build/main/config/armbian.key
 gpg --dearmor < armbian.key > armbian.gpg
 cp armbian.gpg fs-mountpoint/etc/apt/keyrings/
 cp apt/armbian.list fs-mountpoint/etc/apt/sources.list.d/
@@ -35,3 +31,7 @@ chroot fs-mountpoint /root/chroot/debian.sh
 printf "Copying systemd-boot configs..."
 cp systemd-boot/loader.conf fs-mountpoint/efi/loader/
 cp systemd-boot/linux.conf fs-mountpoint/boot/loader/entries/
+
+printf "Configuring networks..."
+cp systemd-networkd/20-wired.network fs-mountpoint/etc/systemd/network/
+ln -sf ../run/systemd/resolve/stub-resolv.conf fs-mountpoint/etc/resolv.conf
