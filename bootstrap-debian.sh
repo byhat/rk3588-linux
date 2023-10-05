cfdfc1ba936fa2d9034e42008d29c05429c8f03f
soyflourbread <soyflourbread@gmail.com>
2023-10-04 19:49:33 -0700

    use echo.

--- bootstrap-debian.sh
+++ bootstrap-debian.sh
@@ -9,12 +9,12 @@ mkdir fs-mountpoint/boot
 mount /dev/loop0p1 fs-mountpoint/efi
 mount /dev/loop0p2 fs-mountpoint/boot
 
-printf "Bootstrapping..."
+echo "Bootstrapping..."
 debootstrap --arch arm64 testing fs-mountpoint http://deb.debian.org/debian/
 
 genfstab -t PARTUUID fs-mountpoint > fs-mountpoint/etc/fstab
 
-printf "Setting up auxiliary repo..."
+echo "Setting up auxiliary repo..."
 wget -O armbian.key https://raw.githubusercontent.com/armbian/build/main/config/armbian.key
 gpg --dearmor < armbian.key > armbian.gpg
 cp armbian.gpg fs-mountpoint/etc/apt/keyrings/
@@ -24,15 +24,15 @@ mount --bind /dev fs-mountpoint/dev
 mount -t proc proc fs-mountpoint/proc
 mount -t sysfs sys fs-mountpoint/sys
 
-printf "Setting up chroot scripts..."
+echo "Setting up chroot scripts..."
 cp -R chroot fs-mountpoint/root/
 
 chroot fs-mountpoint /root/chroot/debian.sh
 
-printf "Copying systemd-boot configs..."
+echo "Copying systemd-boot configs..."
 cp systemd-boot/loader.conf fs-mountpoint/efi/loader/
 cp systemd-boot/linux.conf fs-mountpoint/boot/loader/entries/
 
-printf "Configuring networks..."
+echo "Configuring networks..."
 cp systemd-networkd/20-wired.network fs-mountpoint/etc/systemd/network/
 ln -sf ../run/systemd/resolve/stub-resolv.conf fs-mountpoint/etc/resolv.conf
--- chroot/debian.sh
+++ chroot/debian.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-printf "Executing inside chroot (0)..."
+echo "Executing inside chroot (0)..."
 
-printf "Installing essential packages..."
+echo "Installing essential packages..."
 apt-get update
 apt-get -y install wget btrfs-progs initramfs-tools
 
-printf "Installing Armbian RK3588 kernel..."
+echo "Installing Armbian RK3588 kernel..."
 apt-get update
 apt-get install -y linux-image-edge-rockchip-rk3588 linux-dtb-edge-rockchip-rk3588
 
-printf "Installing systemd components..."
+echo "Installing systemd components..."
 apt-get install -y systemd-boot systemd-resolved
 
-printf "Enabling wired network..."
+echo "Enabling wired network..."
 systemctl enable systemd-networkd
 systemctl enable systemd-resolved
 
-printf "Installing bootloader..."
+echo "Installing bootloader..."
 bootctl --esp-path=/efi --boot-path=/boot install
